5b66d7c9361e51706a14851b1161c7dee9bcc3db
Prasad <prasad.yoaspire@gmail.com>
2023-11-06 17:22:26 +0530

    ecomapp java configuration added.

--- module2/ecomapp/src/main/java/com/cg/ecomapp/AllCustomers.java
+++ module2/ecomapp/src/main/java/com/cg/ecomapp/AllCustomers.java
@@ -2,6 +2,7 @@ package com.cg.ecomapp;
 
 import java.util.List;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cg.ecomapp.controller.CustomerController;
@@ -10,17 +11,18 @@ import com.cg.ecomapp.dto.Customer;
 public class AllCustomers {
 
 	public static void main(String[] args) {
+
+//		ClassPathXmlApplicationContext context = 
+//				new ClassPathXmlApplicationContext("springcontext.xml");
 		
-		ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("springcontext.xml");
-		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
 		CustomerController cController = context.getBean(CustomerController.class);
-		
 		List<Customer> customers = cController.findAllCustomers();
-		
-		customers.forEach(c->System.out.println(c.getCustomerId()+" "+c.getCustomerName()+" "+c.getAge()+" "+c.getEmail()+" "+c.getMobile()));
+
+		customers.forEach(c -> System.out.println(c.getCustomerId() + " " + c.getCustomerName() + " " + c.getAge() + " "
+				+ c.getEmail() + " " + c.getMobile()));
 
 		context.close();
 	}
-
 }
---
+++ module2/ecomapp/src/main/java/com/cg/ecomapp/AppConfig.java
@@ -0,0 +1,50 @@
+package com.cg.ecomapp;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "com.cg.ecomapp")
+@EnableTransactionManagement
+public class AppConfig {
+
+	@Bean
+	public DataSource dataSource(){
+	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	    dataSource.setDriverClassName("org.postgresql.Driver");
+	    dataSource.setUrl("jdbc:postgresql://localhost:5432/cgbatch19db");
+	    dataSource.setUsername("cgbatch19");
+	    dataSource.setPassword("cgbatch19");
+	    return dataSource;
+	}
+	
+	   @Bean
+	   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	      LocalContainerEntityManagerFactoryBean em 
+	        = new LocalContainerEntityManagerFactoryBean();
+	      em.setDataSource(dataSource());
+	      em.setPackagesToScan("com.cg.ecomapp.dto");
+
+	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	      em.setJpaVendorAdapter(vendorAdapter);
+	      em.setJpaProperties(additionalProperties());
+
+	      return em;
+	   }
+	   
+	   Properties additionalProperties() {
+		    Properties properties = new Properties();
+		    properties.setProperty("hibernate.hbm2ddl.auto", "update");		     
+		    return properties;
+		}
+}
